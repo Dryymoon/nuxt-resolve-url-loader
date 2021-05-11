beaa474f18272aa6292d016e96acd0293f512154
dryymoon <igor@pilipenko.cc>
2021-05-11 19:21:46 +0300

    +

--- nuxt-resolve-url-loader.js
+++ nuxt-resolve-url-loader.js
@@ -3,21 +3,22 @@ const traverse = require('traverse');
 module.exports = function (options) {
   this.extendBuild(function setup(config) {
     traverse(config).forEach(function (some) {
-      if (this.key !== 'loader' || some !== 'sass-loader') return;
+      if (this.key === 'loader' && typeof some === 'string' && some.includes('sass-loader')) {
 
-      let loaderConfig = this.parent.node;
-      loaderConfig = { ...loaderConfig, options: { ...loaderConfig.options, sourceMap: true } };
+        let loaderConfig = this.parent.node;
+        loaderConfig = { ...loaderConfig, options: { ...loaderConfig.options, sourceMap: true } };
 
-      this.parent.update(loaderConfig);
+        this.parent.update(loaderConfig);
 
-      const array = [...this.parent.parent.node];
+        const array = [...this.parent.parent.node];
 
-      array.splice(this.parent.path.pop(), 0, {
-        loader: 'resolve-url-loader',
-        options
-      });
+        array.splice(this.parent.path.pop(), 0, {
+          loader: 'resolve-url-loader',
+          options
+        });
 
-      this.parent.parent.update(array);
+        this.parent.parent.update(array);
+      }
     });
   });
 };
